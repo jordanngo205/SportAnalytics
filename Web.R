c9e35140ee9abeea78eee5b4c4883a21ff4b0579
aadeife <adeifeakingbade@yahoo.com>
2024-02-15 23:18:54 -0500

    Merged table into Web and dynamically updated it

--- Web.R
+++ Web.R
@@ -83,6 +83,21 @@ scrape_roster_from_url <- function(short_name) {
   return(list(roster_df1 = roster_df1, roster_df2 = roster_df2))
 }
 
+# Extract player data from dataset
+read_and_process_player_data <- function(team_selected) {
+  players_data <- read_csv("data.csv", show_col_types = FALSE)  # Adjust path to dataset as necessary
+  team_data <- players_data %>%
+    filter(team == team_selected) %>%
+    filter(Player != "* Team Totals") %>%  # Exclude the 'Team Totals*' row
+    arrange(desc(Mpg))
+  
+# Create lists for starters and reserves 
+  list(
+    starters = head(team_data, 5),
+    key_reserves = head(tail(team_data, n = nrow(team_data) - 5), 3)  # Adjusted line
+  )
+}
+
 # Define UI for application with multiple tabs
 ui <- fluidPage(
   
@@ -110,6 +125,221 @@ ui <- fluidPage(
              ),
              fluidRow(
                column(12, tableOutput("top_players_table"))
+             ),
+             tags$head(
+               tags$style(HTML("
+      .table-header {
+        background-color: #FFC000;
+        color: #000000;
+        text-align: center;
+        font-weight: bold;
+        font-size: 20px;
+      }
+      .section-header {
+        background-color: #000000;
+        color: white;
+        text-align: center;
+        font-weight: bold;
+        font-size: 18px;
+      }
+      table {
+        width: 100%;
+        border-collapse: collapse;
+      }
+      th, td {
+        border: 1px solid #dddddd;
+        text-align: center;
+        padding: 8px;
+        vertical-align: middle;
+      }
+      .header-font-size {
+        font-size: 12px;
+      }
+      .data-cell {
+        font-size: 11px;
+      }
+      .stats-divider {
+        position: relative;
+      }
+      .stats-divider:after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 50%;
+        width: 2px;
+        background-color: #000000;
+        height: 100%;
+      }
+      .nested-cell {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+      }
+      .nested-cell > div {
+        width: 100%;
+      }
+
+      .key-reserves-header {
+        background-color: #505050;
+        color: white;
+        font-weight: bold;
+        text-align: center;
+        padding: 8px;
+        border: none;
+        font-size: 18px;
+      }
+      .row-height {
+        height:40px; /* Or any other value */
+      }
+    "))
+             ),
+             titlePanel("Keys to the Game"),
+             tags$table(class = 'table',
+                        tags$thead(
+                          tags$tr(class = "table-header", tags$th(colspan = 4, "PERSONNEL STARTERS")),  
+                        ),
+                        
+                        tags$tbody(
+                          tags$tr( 
+                            tags$th(style = "width: 5%;", "Photo"),  
+                            tags$th(style = "width: 15%;", textOutput("starter1")),  
+                            tags$th(style = "width: 15%;", textOutput("pos_ht1")),  
+                            tags$th(style = "width: 65%;",  textOutput("stats_1"))  
+                          ),
+                          tags$tr(
+                            tags$td(style = "width: 5%;", class="data-cell row-height", ""),
+                            tags$td(style = "width: 15%;", class="data-cell row-height", "Icons"),
+                            tags$td(style = "width: 15%;", class="data-cell row-height", "Shooting Chart"),
+                            tags$td(style = "width: 65%;", class = "stats-divider data-cell", 
+                                    tags$div(class = "nested-cell",
+                                             tags$div("Tendency"),
+                                             tags$div("We Must")
+                                    )
+                            )
+                          ),
+                          tags$tr( 
+                            tags$th(style = "width: 5%;", "Photo"),  
+                            tags$th(style = "width: 15%;", class="data-cell row-height", textOutput("starter2")),  
+                            tags$th(style = "width: 15%;", class="data-cell row-height", textOutput("pos_ht2")),  
+                            tags$th(style = "width: 65%;", class="data-cell row-height",  textOutput("stats_2"))  
+                          ),
+                          tags$tr(
+                            tags$td(style = "width: 5%;", class="data-cell custom-height", ""),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Icons"),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Shooting Chart"),
+                            tags$td(style = "width: 65%;", class = "stats-divider data-cell", 
+                                    tags$div(class = "nested-cell",
+                                             tags$div("Tendency"),
+                                             tags$div("We Must")
+                                    )
+                            )
+                          ),               tags$tr( 
+                            tags$th(style = "width: 5%;", "Photo"),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("starter3")),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("pos_ht3")),  
+                            tags$th(style = "width: 65%;", class="data-cell custom-height",  textOutput("stats_3"))  
+                          ),
+                          tags$tr(
+                            tags$td(style = "width: 5%;", class="data-cell custom-height", ""),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Icons"),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Shooting Chart"),
+                            tags$td(style = "width: 65%;", class = "stats-divider data-cell", 
+                                    tags$div(class = "nested-cell",
+                                             tags$div("Tendency"),
+                                             tags$div("We Must")
+                                    )
+                            )
+                          ),               tags$tr( 
+                            tags$th(style = "width: 5%;", "Photo"),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("starter4")),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("pos_ht4")),  
+                            tags$th(style = "width: 65%;", class="data-cell custom-height",  textOutput("stats_4"))  
+                          ),
+                          tags$tr(
+                            tags$td(style = "width: 5%;", class="data-cell custom-height", ""),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Icons"),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Shooting Chart"),
+                            tags$td(style = "width: 65%;", class = "stats-divider data-cell", 
+                                    tags$div(class = "nested-cell",
+                                             tags$div("Tendency"),
+                                             tags$div("We Must")
+                                    )
+                            )
+                          ),               tags$tr( 
+                            tags$th(style = "width: 5%;", "Photo"),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("starter5")),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("pos_ht5")),  
+                            tags$th(style = "width: 65%;", class="data-cell custom-height",  textOutput("stats_5"))  
+                          ),
+                          tags$tr(
+                            tags$td(style = "width: 5%;", class="data-cell custom-height", ""),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Icons"),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Shooting Chart"),
+                            tags$td(style = "width: 65%;", class = "stats-divider data-cell", 
+                                    tags$div(class = "nested-cell",
+                                             tags$div("Tendency"),
+                                             tags$div("We Must")
+                                    )
+                            )
+                          ),
+                          
+                          # Repeat the above tr for as many rows as you need
+                          # After the last row of players, add the Key Reserves section header
+                          tags$tr(
+                            tags$th(class = "key-reserves-header", colspan = 4, "KEY RESERVES")
+                          ),
+                          tags$tr( 
+                            tags$th(style = "width: 5%;", "Photo"),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("reserve1")),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("pos_ht_reserve1")),  
+                            tags$th(style = "width: 65%;", class="data-cell custom-height", textOutput("reserve_stats_1")),  
+                          ),
+                          tags$tr(
+                            tags$td(style = "width: 5%;", class="data-cell custom-height", ""),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Icons"),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Shooting Chart"),
+                            tags$td(style = "width: 65%;", class = "stats-divider data-cell", 
+                                    tags$div(class = "nested-cell",
+                                             tags$div("Tendency"),
+                                             tags$div("We Must")
+                                    )
+                            )
+                          ),               tags$tr( 
+                            tags$th(style = "width: 5%;", "Photo"),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("reserve2")),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("pos_ht_reserve2")),  
+                            tags$th(style = "width: 65%;", class="data-cell custom-height", textOutput("reserve_stats_2")),  
+                          ),
+                          tags$tr(
+                            tags$td(style = "width: 5%;", class="data-cell custom-height", ""),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Icons"),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Shooting Chart"),
+                            tags$td(style = "width: 65%;", class = "stats-divider data-cell", 
+                                    tags$div(class = "nested-cell",
+                                             tags$div("Tendency"),
+                                             tags$div("We Must")
+                                    )
+                            )
+                          ),               tags$tr( 
+                            tags$th(style = "width: 5%;", "Photo"),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("reserve3")),  
+                            tags$th(style = "width: 15%;", class="data-cell custom-height", textOutput("pos_ht_reserve3")),  
+                            tags$th(style = "width: 65%;", class="data-cell custom-height", textOutput("reserve_stats_3")),  
+                          ),
+                          tags$tr(
+                            tags$td(style = "width: 5%;", class="data-cell custom-height", ""),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Icons"),
+                            tags$td(style = "width: 15%;", class="data-cell custom-height", "Shooting Chart"),
+                            tags$td(style = "width: 65%;", class = "stats-divider data-cell", 
+                                    tags$div(class = "nested-cell",
+                                             tags$div("Tendency"),
+                                             tags$div("We Must")
+                                    )
+                            )
+                          ),
+                          # Repeat the above tr for as many reserve rows as needed
+                        )
              )
     )
   )
@@ -210,6 +440,99 @@ server <- function(input, output) {
   # Render the selected team name for the Scouting Report tab
   output$team_name <- renderText({
     roster_data$team_name
+  })
+  
+  # Reactive expression to fetch and store player data
+  reactive_player_data <- reactive({
+    team_selected <- input$team  # Assuming there's an input in UI to select team
+    short_name <- team_lookup[team_selected]
+    if (!is.na(short_name)) {
+      read_and_process_player_data(short_name)
+    } else {
+      list(starters = NULL, key_reserves = NULL)
+    }
+  })
+  
+  # Assuming reactive_player_data() fetches and processes the data
+  # Dynamically populate scouting report table
+  observe({
+    player_data <- reactive_player_data()
+    starters <- player_data$starters
+    key_reserves <- player_data$key_reserves
+    
+    # Populate starters
+    if (!is.null(starters) && nrow(starters) >= 5) {
+      output$starter1 <- renderText({ starters$Player[1] })
+      output$starter2 <- renderText({ starters$Player[2] })
+      output$starter3 <- renderText({ starters$Player[3] })
+      output$starter4 <- renderText({ starters$Player[4] })
+      output$starter5 <- renderText({ starters$Player[5] })
+    }
+    
+    # Populate key reserves
+    if (!is.null(key_reserves) && nrow(key_reserves) >= 3) {
+      output$reserve1 <- renderText({ key_reserves$Player[1] })
+      output$reserve2 <- renderText({ key_reserves$Player[2] })
+      output$reserve3 <- renderText({ key_reserves$Player[3] })
+    }
+    
+    # Populate starters' POS-H/T
+    if (!is.null(starters) && nrow(starters) >= 5) {
+      output$pos_ht1 <- renderText({ paste(starters$Position[1], ",", starters$Ht[1]) })
+      output$pos_ht2 <- renderText({ paste(starters$Position[2], ",", starters$Ht[2]) })
+      output$pos_ht3 <- renderText({ paste(starters$Position[3], ",", starters$Ht[3]) })
+      output$pos_ht4 <- renderText({ paste(starters$Position[4], ",", starters$Ht[4]) })
+      output$pos_ht5 <- renderText({ paste(starters$Position[5], ",", starters$Ht[5]) })
+    }
+    
+    # Populate key reserves' POS-H/T in a similar manner
+    if (!is.null(key_reserves) && nrow(key_reserves) >= 3) {
+      output$pos_ht_reserve1 <- renderText({ paste(key_reserves$Position[1], ",", key_reserves$Ht[1]) })
+      output$pos_ht_reserve2 <- renderText({ paste(key_reserves$Position[2], ",", key_reserves$Ht[2]) })
+      output$pos_ht_reserve3 <- renderText({ paste(key_reserves$Position[3], ",", key_reserves$Ht[3]) })
+    }
+    
+    # Pull key stats based on position
+    format_stats <- function(position, stats) {
+      if (position %in% c("Forward", "Post")) {
+        # Format for Forward/Post: PPG, RPG, FT%
+        paste(stats$PPG, "PPG,", stats$RPG, "RPG,", stats$`FT%`, "FT")
+      } 
+      else if (position %in% c("Guard", "Wing", "Guard/Forward")) {
+        # Format for Guard/Wing/Guard-Forward: PPG, 3P%, FT%
+        paste(stats$PPG, "PPG,", stats$`3P%`, "3P%,", stats$`FT%`, "FT")
+      } 
+      else {
+        "Stats Not Available"
+      }
+    }
+    
+    # Populate stats boxes for starters
+    if (!is.null(starters) && nrow(starters) >= 5) {
+      for (i in 1:5) {
+        local({
+          idx <- i  # Localize iterator for reactive context
+          output[[paste0("stats_", idx)]] <- renderText({
+            # Assuming format_stats formats stats based on the player's position
+            format_stats(starters$Position[idx], starters[idx, ])
+          })
+        })
+      }
+    }
+    
+    # Populate stats boxes for reserves
+    if (!is.null(key_reserves) && nrow(key_reserves) >= 3) {
+      for (i in 1:3) {  # Assuming you have 3 key reserves
+        local({
+          idx <- i  # Localize iterator for reactive context
+          output[[paste0("reserve_stats_", idx)]] <- renderText({
+            # Assuming format_stats formats stats based on the player's position
+            format_stats(key_reserves$Position[idx], key_reserves[idx, ])
+          })
+        })
+      }
+    }
+    
   })
 }
 
